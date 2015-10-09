9dbe430254c734d844574676a02455b8aaf0ecb3
Ivan Chau <ivanmchau@gmail.com>
2015-10-09 19:44:59 -0400

    synta

--- app.js
+++ app.js
@@ -111,7 +111,7 @@ app.get('/search/query', function(req,res){
           var main = JSON.parse(body);
           if (!main['collection'][0]){
             for (tracks in main['collection']){
-              if (main['collection'][tracks] && ){
+              if (main['collection'][tracks]){
 
                 song1 = main['collection'][tracks]['permalink_url'];
                 break;
