97f2cd93f2d168d1688dcdf04a9ef6601c50e128
Ivan Chau <ivanmchau@gmail.com>
2015-10-11 15:11:11 -0400

    GO

--- app.js
+++ app.js
@@ -134,7 +134,15 @@ app.get('/search/query', function(req,res){
                 song1 = main['collection'][0]['permalink_url'];
             }else if (main['collection'][0]['kind'] == 'track'){
               if(main['collection'][0]['embeddable_by'] == 'all'){
-                song1 = main['collection'][tracks]['permalink_url'];
+                song1 = main['collection'][0]['permalink_url'];
+                break;
+              }else{
+                for(tracks in main['collection']){
+                  if(main['collection'][tracks] && main['collection'][tracks]['embeddable_by'] == 'all'){
+                    song1 = main['collection'][tracks]['permalink_url'];
+                    break;
+                  }
+                }
               }
             }
           }else{
