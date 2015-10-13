302274fea27077be137a30c15779f85c9810c020
Ivan Chau <ivanmchau@gmail.com>
2015-10-13 14:39:36 -0400

    embed_url?

--- app.js
+++ app.js
@@ -180,7 +180,8 @@ app.get('/search/query', function(req,res){
           console.log(mi);
           var item = mi['data'];
           if(item[0]){
-            var great = '"' + item[0]['images']['downsized_large']['url'] + '"';
+            //var great = '"' + item[0]['images']['downsized_large']['url'] + '"';
+            var great = '"' + item[0]['embed_url'] + '"';
             console.log(great);
             image.push(great);
             red = true;
