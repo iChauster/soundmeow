e913b3aa81c2a6e514f47ba8d5cf306de559f19e
Ivan Chau <ivanmchau@gmail.com>
2015-10-13 19:43:13 -0400

    additional route

--- routes/playlist.js
+++ routes/playlist.js
@@ -1,4 +1,4 @@
-
+module.exports = function(app){
 // FIND
 app.get('/find/:paramID1/:paramID2', function (req, res) {
   console.log('inside ====================================');
@@ -169,4 +169,4 @@ function longest(song1, song2){
         })
   playlistOps()
 })
-module.exports.app = app;
+}
